afb76856075c2eb65c85fa2948b09193c1140231
Adriel Radicchi <adriel.nfsc@gmail.com>
2017-12-06 21:38:12 -0200

    Update
    Update  method filter_odd_squares

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -41,7 +41,7 @@ defmodule Identicon do
   end
 
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
-    Enum.filter grid, fn({code, _index}) -> 
+    grid = Enum.filter grid, fn({code, _index}) -> 
       rem(code, 2) == 0 
     end
     
@@ -51,16 +51,17 @@ defmodule Identicon do
   def build_grid(%Identicon.Image{hex: hex} = image) do
     grid = 
       hex
-        |> Enum.chunk(3)
-        |> Enum.map(&mirror_row/1)
-        |> List.flatten
-        |> Enum.with_index
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
 
     %Identicon.Image{image | grid: grid}
   end
 
   def mirror_row(row) do
     [first, second | _tail] = row
+
     row ++ [second, first]
   end
 
